832fcd6d02ed795606f68bd848ce425147df3b39
Ankit Bajpai <ankit.bajpai.ext@flydubai.com>
2017-12-17 16:33:18 +0530

    Prism QuickStart reference code

---
+++ 3.Interactivity QuickStart/C#/InteractivityQuickstart/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+
+using System.Windows;
+using InteractivityQuickstart.ViewModels;
+
+namespace InteractivityQuickstart
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
